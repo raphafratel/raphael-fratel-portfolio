597b26fb99574dbe15ff4916acb006bfdaaa8bc1
Raphael Fratel <raphael.fratel@gmail.com>
2024-04-22 15:19:19 -0300

    Implemented firts styles and animations in header component

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,15 +1,34 @@
 "use client";
 
 import React from 'react'
-import './style.css'
 import { motion } from "framer-motion";
+import { links } from '@/lib/data'
+import Link from 'next/link';
+import './style.css'
 
 export default function header() {
   return (
     <header className='header-style'>
 
-      <motion.div className='navbar-style'>
-        </motion.div>
+      <motion.div className='navbar-bg-style'
+        initial={{ y: -100, x: '-50%', opacity: 0 }}
+        animate={{ y: 0, x: '-50%', opacity: 1 }}>
+      </motion.div>
+
+      <nav className='navbar-style'>
+        <ul className='nav-ul-style'>
+          {links.map((link) => (
+            <motion.li className='nav-li-style' key={link.hash}
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+            >
+              <Link className='nav-link-style' href={link.hash}>
+                {link.name}
+              </Link>
+            </motion.li>
+          ))}
+        </ul>
+      </nav>
 
     </header>
   )
